3637e1bed6829262a796cc715cf504843e828df8
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2024-09-08 01:40:40 +0530

    Added all projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,36 @@ import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 import { ModeToggle } from "../components/ModeToggle";
 import ProjectCard from "../components/ProjectCard";
-import { projects } from "../lib/constants";
+import { projects, skillIconsKeywords } from "../lib/constants";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col items-center pb-6">
       <div className="text-center mt-5 text-xl font-light font-mono">
         <p>Hello There !</p>
         <p>I'm Charan</p>
       </div>
       <ModeToggle className="absolute top-5 right-5" />
-      <p>Projects:</p>
       <br />
-      <div className="px-5 flex flex-wrap gap-5">
-        {projects.map((pjt, i) => (
-          <ProjectCard project={pjt} key={i} />
-        ))}
+      <div>
+        <p className="text-xl">Technologies I've worked on:</p>
+        <br />
+        <img
+          src={`https://skillicons.dev/icons?i=${Object.values(
+            skillIconsKeywords
+          ).join(",")}`}
+          className="pl-8"
+        />
+      </div>
+      <br />
+      <div className="px-16">
+        <p className="text-xl">Projects:</p>
+        <br />
+        <div className="px-5 flex flex-wrap gap-5 justify-evenly">
+          {projects.map((pjt, i) => (
+            <ProjectCard project={pjt} key={i} />
+          ))}
+        </div>
       </div>
     </div>
   );
